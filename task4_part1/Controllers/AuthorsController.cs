862bc1f8ba48b3d5dbc3d6db4de63f2b69a593d6
user <user@WIN-0FLLSABRR5D>
2025-10-18 23:37:12 +0300

    создала контроллеры

---
+++ task4_part1/Controllers/AuthorsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace task4_part1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<>
+    }
+}
