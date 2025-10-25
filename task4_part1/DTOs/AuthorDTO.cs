2d4f418e965fd5b9af1599d06f6fa7e044f3e843
user <user@WIN-0FLLSABRR5D>
2025-10-25 16:57:27 +0300

    добавила DTO

---
+++ task4_part1/DTOs/AuthorDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace task4_part1.DTOs
+{
+    public class AuthorDTO
+    {
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "Укажите имя автора")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Укажите фамилию автора")]
+        public string? LastName { get; set; }
+
+        [Required(ErrorMessage = "Укажите дату рождения автора")]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ task4_part1/DTOs/BookDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace task4_part1.DTOs
+{
+    public class BookDTO
+    {
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "Название книги обязательно")]
+        public string? Title { get; set; }
+        public DateTime PublishedYear { get; set; }
+
+        [Required(ErrorMessage = "Необходимо указать автора")]
+        public long AuthorId { get; set; }
+        public string? AuthorName { get; set; }
+    }
+}
